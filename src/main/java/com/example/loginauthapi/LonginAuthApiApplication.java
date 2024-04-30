7b6335d1dbad99a652128532c7ba0a9aae6460f6
Ricardo Chiarelli <ricardo.almeidasantos04@gmail.com>
2024-04-30 19:45:20 -0300

    Initial commit

---
+++ src/main/java/com/example/loginauthapi/LonginAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonginAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonginAuthApiApplication.class, args);
+	}
+
+}
